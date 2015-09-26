b9ed29e6bb6e3bb904d41132ca1894df7610855f
Chris <chrisjcw@gmail.com>
2015-09-26 12:44:49 +0100

    Created animal model

---
+++ models/animals.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose')
+
+var petSchema = new mongoose.Schema({ // send the 'schema' to mongoose
+  name: { type: String, required: true, unique: true }
+  breed: String,
+  dob: Date,
+  gender: String,
+  family: String,
+  status: String,
+  createdAt: Date,
+  updatedAt: { type: Date, default: Date.now }
+})
+
+var Pet = mongoose.model('Pet', petSchema)
+
+module.exports = Pet
